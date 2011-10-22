1653ed5f6250eff4e9541cad70f6d3f092b29a02
Jake Alheid <shakefu@gmail.com>
2011-10-22 01:11:18 +0000

    Add setup and empty package.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+        name='foo',
+        version='0.0.1',
+        description='Eric WSGI Framework',
+        author='Jake Alheid',
+        author_email='jacob.alheid@gmail.com',
+        url='http://about.me/jake/',
+        packages=['eric'],
+)
+
