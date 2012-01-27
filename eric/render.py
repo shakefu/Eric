c6b3955513963472673456bb68d776faccbc6b32
Jake Alheid <shakefu@gmail.com>
2012-01-27 15:36:23 -0800

    Work continues

---
+++ eric/render.py
@@ -0,0 +1,25 @@
+import tenjin
+from webext import (escape, escape_html, set_encoding, to_str, get_encoding)
+from tenjin.helpers import (cache_as, capture_as, CaptureContext, captured_as,
+        echo, echo_cached, fragment_cache, html, new_cycle, re, start_capture,
+        stop_capture, sys, unquote, not_cached)
+
+class TenjinTemplate(tenjin.Template):
+    preamble = ';'.join((
+        "import pprint",
+        "globals()['pprint'] = pprint",
+        "globals()['_context'] = _context",
+        "base_template = lambda name: _context.__setitem__('_layout', name)",
+        "debug_context = lambda: pprint.pprint(_context)",
+        ))
+
+# Configure Tenjin
+tenjin.Engine.templateclass = TenjinTemplate
+TenjinTemplate.engine = tenjin.Engine(path=['.'])
+
+
+def with_tenjin(context=None, template=None):
+    assert template
+    context = context or {}
+    return TenjinTemplate.engine.render(template, context)
+
--- eric/wsgi.py
+++ eric/wsgi.py
@@ -2,34 +2,45 @@
 WSGI Framework
 
 """
+import re
+import sys
 import pprint
 import urlparse
 import importlib
+import functools
 
 import webob
 import webob.exc
 
-import tmp.app
+import eric.render
 
+
+LOAD_APPS = [
+        'tmp.app',
+        'eric.render',
+        ]
 
 WSGI_MIDDLEWARE = [
-        # 'eric.wsgi.debug_env',
+        'eric.wsgi.debug_env',
         'eric.wsgi.route',
         ]
 
 WSGI_MIDDLEWARE_CACHE = []
 
+ROUTES = []
 
-class WriteOnceDict(dict):
-    """ Dict-like class that only allows setting of keys once. """
-    def __setitem__(self, item, value):
-        if item in self:
-            raise KeyError("Key already exists")
-        return dict.__setitem__(self, item, value)
+
+def load_apps():
+    if LOAD_APPS:
+        while LOAD_APPS:
+            app = LOAD_APPS.pop()
+            sys.modules[app] = importlib.import_module(app)
 
 
 def entry(environ, start_response):
     """ Entry point into WSGI glue. """
+    load_apps()
+
     if not WSGI_MIDDLEWARE_CACHE:
         for middleware in WSGI_MIDDLEWARE:
             module, middleware = middleware.rsplit('.', 1)
@@ -42,8 +53,7 @@ def entry(environ, start_response):
         if response:
             return response
 
-    start_response('500 Internal Server Error', [('Content-type', 'text/html')])
-    return "500 Internal Server Error"
+    return webob.exc.HTTPServerError()(environ, start_response)
 
 
 def debug_env(environ, start_response):
@@ -52,21 +62,62 @@ def debug_env(environ, start_response):
 
 def route(environ, start_response):
     if 'PATH_INFO' not in environ:
-        environ['PATH_INFO'] = '/'
+        environ['PATH_INFO'] = ''
 
-    request = webob.Request(environ)
-    try:
-        response = tmp.app.view(request)
-    except webob.exc.HTTPException, exc:
-        response = exc
-    else:
-        response = webob.Response(response)(environ, start_response)
+    path = environ['PATH_INFO']
+    path = path[1:] if path.startswith('/') else path
 
-    return response
+    response = None
+    for regex, method, handler in ROUTES:
+        if method and method != environ['REQUEST_METHOD']:
+            continue
+        if regex.match(path):
+            response = handler(environ, start_response)
+            break
 
-    # start_response('200 OK', [('Content-type', 'text/html')])
-    # return tmp.app.render('test.pyhtml', {'title':'Test', 'items':xrange(2)})
+    if response:
+        return response
 
-    start_response('404 Not Found', [('Content-type', 'text/html')])
-    return "404 Not Found"
+    return webob.exc.HTTPNotFound()(environ, start_response)
+
+
+class request(object):
+    def __init__(self, pattern, template=None, method=None,
+            render=eric.render.with_tenjin):
+        self.pattern = pattern
+        self.template = template
+        self.method = method
+        self.render = render
+
+    def __call__(self, func):
+        if not self.template:
+            self.template = func.func_name + '.pyhtml'
+
+        self.func = func
+        self.routes.append((re.compile(self.pattern), self.method, self.handle))
+
+        return func
+
+    def handle(self, environ, start_response):
+        req = webob.Request(environ)
+        context = self.func(req)
+        body = self.render(context=context, template=self.template)
+        return webob.Response(body)(environ, start_response)
+
+    @property
+    def routes(self):
+        return ROUTES
+
+    @classmethod
+    def GET(cls, pattern, template=None):
+        return cls(pattern, template, 'GET')
+
+    @classmethod
+    def POST(cls, pattern, template=None):
+        return cls(pattern, template, 'POST')
+
+
+def register_route(pattern, method, handler):
+    request.routes.append(re.compile(pattern), method, handler)
+
 
