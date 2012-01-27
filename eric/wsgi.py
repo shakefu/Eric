c9b5e4e2e008b567eb757eed0af1fe5a4f1669a6
Jake Alheid <shakefu@gmail.com>
2012-01-26 21:22:00 -0800

    Work continues

--- eric/wsgi.py
+++ eric/wsgi.py
@@ -2,24 +2,33 @@
 WSGI Framework
 
 """
-
 import pprint
 import urlparse
 import importlib
+
+import webob
+import webob.exc
 
 import tmp.app
 
 
 WSGI_MIDDLEWARE = [
-        'eric.wsgi.formdecode',
-        'eric.wsgi.debug_env',
+        # 'eric.wsgi.debug_env',
         'eric.wsgi.route',
         ]
 
 WSGI_MIDDLEWARE_CACHE = []
 
 
-def entry(env, app):
+class WriteOnceDict(dict):
+    """ Dict-like class that only allows setting of keys once. """
+    def __setitem__(self, item, value):
+        if item in self:
+            raise KeyError("Key already exists")
+        return dict.__setitem__(self, item, value)
+
+
+def entry(environ, start_response):
     """ Entry point into WSGI glue. """
     if not WSGI_MIDDLEWARE_CACHE:
         for middleware in WSGI_MIDDLEWARE:
@@ -28,36 +37,36 @@ def entry(env, app):
             middleware = getattr(module, middleware)
             WSGI_MIDDLEWARE_CACHE.append(middleware)
 
-    # TODO: Wrap env in a dict-like object that doesn't allow overwriting of
-    # existing keys
-
     for middleware in WSGI_MIDDLEWARE_CACHE:
-        response = middleware(env, app)
+        response = middleware(environ, start_response)
         if response:
             return response
 
-    app('500 Internal Server Error', [('Content-type', 'text/html')])
+    start_response('500 Internal Server Error', [('Content-type', 'text/html')])
     return "500 Internal Server Error"
 
 
-def debug_env(env, app):
-    pprint.pprint(env)
+def debug_env(environ, start_response):
+    pprint.pprint(environ)
 
 
-def formdecode(env, app):
-    # TODO: Assign these to variables that make more sense (e.g. not HTTP-methods)
-    # Maybe "data" and "query"?
-    env['GET'] = urlparse.parse_qs(env['QUERY_STRING'])
-    env['POST'] = urlparse.parse_qs(env['wsgi.input'].read())
+def route(environ, start_response):
+    if 'PATH_INFO' not in environ:
+        environ['PATH_INFO'] = '/'
 
+    request = webob.Request(environ)
+    try:
+        response = tmp.app.view(request)
+    except webob.exc.HTTPException, exc:
+        response = exc
+    else:
+        response = webob.Response(response)(environ, start_response)
 
-def route(env, app):
-    if 'PATH_INFO' not in env:
-        env['PATH_INFO'] = '/'
+    return response
 
-    app('200 OK', [('Content-type', 'text/html')])
-    return tmp.app.render('test.pyhtml', {'title':'Test', 'items':xrange(2)})
+    # start_response('200 OK', [('Content-type', 'text/html')])
+    # return tmp.app.render('test.pyhtml', {'title':'Test', 'items':xrange(2)})
 
-    app('404 Not Found', [('Content-type', 'text/html')])
+    start_response('404 Not Found', [('Content-type', 'text/html')])
     return "404 Not Found"
 
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
         name='eric',
-        version='0.0.2',
+        version='0.0.3',
         description='Eric WSGI Framework',
         author='Jake Alheid',
         author_email='jacob.alheid@gmail.com',
@@ -14,6 +14,8 @@ setup(
             'gevent >= 0.13.6',
             'tenjin >= 1.0.2',
             'webext >= 0.0.1',
+            'minimongo >= 0.2.7',
+            'webob',
             ]
 )
 
